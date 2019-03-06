4e2c72455cf4eb18ba7f1a05d5188bb075ea2e71
Colin Z. Robertson <czr@rtnl.org.uk>
2019-03-06 14:38:28 +0000

    Tidy code

--- beeminder.js
+++ beeminder.js
@@ -1,32 +1,30 @@
+'use strict'
+
 const axios = require('axios')
 const qs = require('qs')
 
-async function getJson(url) {
-  const response = await axios.get(url)
-
-  return response.data
-}
-
 function goal(user, authToken, goal) {
-  return {
+  const apiBase = 'https://www.beeminder.com/api/v1'
 
+  return {
     datapoints: async () => {
-      url = `https://www.beeminder.com/api/v1/users/${user}/goals/${goal}/datapoints.json?auth_token=${authToken}`
-      return getJson(url)
+      let url = `${apiBase}/users/${user}/goals/${goal}/datapoints.json?auth_token=${authToken}`
+      let response = await axios.get(url)
+      return response.data
     },
 
     createDatapoint: async (datapoint) => {
-      url = `https://www.beeminder.com/api/v1/users/${user}/goals/${goal}/datapoints.json`
-      result = await axios.post(
+      let url = `${apiBase}/users/${user}/goals/${goal}/datapoints.json`
+      let response = await axios.post(
         url,
         qs.stringify({ ...datapoint, auth_token: authToken }),
         { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
       )
-      return result.data.id
+      return response.data.id
     },
 
     updateDatapoint: async (datapoint) => {
-      url = `https://www.beeminder.com/api/v1/users/${user}/goals/${goal}/datapoints/${datapoint.id}.json`
+      let url = `${apiBase}/users/${user}/goals/${goal}/datapoints/${datapoint.id}.json`
       return axios.put(
         url,
         qs.stringify({ ...datapoint, auth_token: authToken }),
@@ -35,7 +33,7 @@ function goal(user, authToken, goal) {
     },
 
     deleteDatapoint: async (id) => {
-      url = `https://www.beeminder.com/api/v1/users/${user}/goals/${goal}/datapoints/${id}.json?auth_token=${authToken}`
+      let url = `${apiBase}/users/${user}/goals/${goal}/datapoints/${id}.json?auth_token=${authToken}`
       return axios.delete(url)
     },
 
