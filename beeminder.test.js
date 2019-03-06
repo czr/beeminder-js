fc2d57dfc61ac6e60e5b32b188a69dc9a4b9aec5
Colin Z. Robertson <czr@rtnl.org.uk>
2019-03-06 14:39:14 +0000

    Add basic test for createDatapoint

--- beeminder.test.js
+++ beeminder.test.js
@@ -4,7 +4,7 @@ const mockAxios = require("axios")
 const beeminder = require("./beeminder")
 
 describe("beeminder goal", () => {
-  it("fetches datapoints from API", async () => {
+  it("fetches datapoints", async () => {
     var expectedDatapoints = [
       {
         "id": "1",
@@ -42,6 +42,36 @@ describe("beeminder goal", () => {
     expect(mockAxios.get).toHaveBeenCalledTimes(1)
     expect(mockAxios.get).toHaveBeenCalledWith(
       "https://www.beeminder.com/api/v1/users/test-user/goals/test-goal/datapoints.json?auth_token=XXXXXXXX",
+    )
+  })
+
+
+  it("creates datapoints", async () => {
+    var expectedId = 1234
+    mockAxios.post.mockImplementationOnce(() =>
+      Promise.resolve({
+        data: { id: expectedId },
+      })
+    )
+
+    var goal = beeminder.goal(
+      'test-user',
+      'XXXXXXXX',
+      'test-goal'
+    )
+    var datapoint = {
+      "daystamp": "20090213",
+      "value": 5,
+      "comment": "Test datapoint",
+    }
+    var gotId = await goal.createDatapoint()
+
+    expect(gotId).toEqual(expectedId)
+    expect(mockAxios.post).toHaveBeenCalledTimes(1)
+    expect(mockAxios.post).toHaveBeenCalledWith(
+      "https://www.beeminder.com/api/v1/users/test-user/goals/test-goal/datapoints.json",
+      expect.any(String),
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
     )
   })
 })
